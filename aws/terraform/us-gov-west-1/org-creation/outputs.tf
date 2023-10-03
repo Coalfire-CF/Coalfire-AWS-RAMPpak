7a63f8fd42c74425903a5ae4e95a632bd8b3ab40
kmobl <moblkourosh@gmail.com>
2023-10-03 14:36:40 -0500

    fixing some issues in final build bpr

---
+++ aws/terraform/us-gov-west-1/org-creation/outputs.tf
@@ -0,0 +1,28 @@
+output "accounts" {
+  value = module.org.accounts
+  description = "List of org accounts including master"
+}
+
+output "org_roots" {
+  value = module.org.org_roots
+}
+
+output "org_arn" {
+  value = module.org.org_arn
+}
+
+output "org_id" {
+  value = module.org.org_id
+}
+
+output "master_account_arn" {
+  value = module.org.master_account_arn
+}
+
+output "ou_arn" {
+  value = aws_organizations_organizational_unit.ou.arn
+}
+
+output "ou_id" {
+  value = aws_organizations_organizational_unit.ou.id
+}
